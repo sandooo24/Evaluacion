61d5dfb2a9e7b5e9c2e7639b8e7120cf50eddff5
sandooo24 <163065420+sandooo24@users.noreply.github.com>
2024-06-24 21:07:16 -0300

    Update header.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 	<ul class="nav-menu">
 		<li class="nav-menu-item"><a href="admin.php">Inicio</a></li>
 		<li class="nav-menu-item"><a href="listado.php">Ver Reportes</a></li>
-		<li class="nav-menu-item"><a href="selecionarano.php">Estadisticas</a></li>
+		<li class="nav-menu-item"><a href="graficoReportes.php">Estadisticas</a></li>
 		<li class="nav-menu-item"><a href="./">Cerrar Sesión</a></li>
 	</ul>
 </nav>
